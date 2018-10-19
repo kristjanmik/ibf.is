003e480c4d8a7e536e47b4ad715c1919b520ce2b
David Bachmann <dabbi@dabbi.is>
2018-10-19 11:08:30 +0000

    Draft typography sizing. Prepare Avatars component and data.

---
+++ src/components/Avatars/Avatars.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Avatars.module.scss";
+
+const Avatars = ({}) => (
+  <div className={styles.root}>
+  </div>
+)
+
+Avatars.propTypes = {
+};
+
+export default Avatars;
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,35 +4,125 @@ export const availableWorkingGroups = [
     leader: "Einar Eymundsson",
     description:
       "The developer group hosts regular meetups on blockchain implementations, cryptography and other related topics",
+    icon: "//placehold.it/150x150",
+    members: [
+      {
+        name: "Einar Eymundsson",
+        image: "https://randomuser.me/api/portraits/men/43.jpg",
+      },
+      {
+        name: "Kristján Mikaelsson",
+        image: "https://randomuser.me/api/portraits/men/79.jpg",
+      },
+      {
+        name: "Vigdís Finnbogadóttir",
+        image: "https://randomuser.me/api/portraits/women/48.jpg",
+      },
+    ],
   },
   {
     name: "Mining",
     leader: "Atli Óðinsson",
     description:
       "The mining group works on educating people on how to set up their mining equipment. Meets up every two months.",
+    icon: "//placehold.it/150x150",
+    members: [
+      {
+        name: "Einar Eymundsson",
+        image: "https://randomuser.me/api/portraits/men/43.jpg",
+      },
+      {
+        name: "Kristján Mikaelsson",
+        image: "https://randomuser.me/api/portraits/men/79.jpg",
+      },
+      {
+        name: "Vigdís Finnbogadóttir",
+        image: "https://randomuser.me/api/portraits/women/48.jpg",
+      },
+    ],
   },
   {
     name: "Legislation",
     leader: "Einar Eymundsson",
+    icon: "//placehold.it/150x150",
     description:
       "The developer group hosts regular meetups on blockchain implementations, cryptography and other related topics",
+    members: [
+      {
+        name: "Einar Eymundsson",
+        image: "https://randomuser.me/api/portraits/men/43.jpg",
+      },
+      {
+        name: "Kristján Mikaelsson",
+        image: "https://randomuser.me/api/portraits/men/79.jpg",
+      },
+      {
+        name: "Vigdís Finnbogadóttir",
+        image: "https://randomuser.me/api/portraits/women/48.jpg",
+      },
+    ],
   },
   {
     name: "PR and Social",
     leader: "Einar Eymundsson",
+    icon: "//placehold.it/150x150",
     description:
       "The developer group hosts regular meetups on blockchain implementations, cryptography and other related topics",
+    members: [
+      {
+        name: "Einar Eymundsson",
+        image: "https://randomuser.me/api/portraits/men/43.jpg",
+      },
+      {
+        name: "Kristján Mikaelsson",
+        image: "https://randomuser.me/api/portraits/men/79.jpg",
+      },
+      {
+        name: "Vigdís Finnbogadóttir",
+        image: "https://randomuser.me/api/portraits/women/48.jpg",
+      },
+    ],
   },
   {
     name: "Education",
     leader: "Einar Eymundsson",
+    icon: "//placehold.it/150x150",
     description:
       "The developer group hosts regular meetups on blockchain implementations, cryptography and other related topics",
+    members: [
+      {
+        name: "Einar Eymundsson",
+        image: "https://randomuser.me/api/portraits/men/43.jpg",
+      },
+      {
+        name: "Kristján Mikaelsson",
+        image: "https://randomuser.me/api/portraits/men/79.jpg",
+      },
+      {
+        name: "Vigdís Finnbogadóttir",
+        image: "https://randomuser.me/api/portraits/women/48.jpg",
+      },
+    ],
   },
   {
     name: "Community",
     leader: "Einar Eymundsson",
+    icon: "//placehold.it/150x150",
     description:
       "The developer group hosts regular meetups on blockchain implementations, cryptography and other related topics",
+    members: [
+      {
+        name: "Einar Eymundsson",
+        image: "https://randomuser.me/api/portraits/men/43.jpg",
+      },
+      {
+        name: "Kristján Mikaelsson",
+        image: "https://randomuser.me/api/portraits/men/79.jpg",
+      },
+      {
+        name: "Vigdís Finnbogadóttir",
+        image: "https://randomuser.me/api/portraits/women/48.jpg",
+      },
+    ],
   },
 ];
