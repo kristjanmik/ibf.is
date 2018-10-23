d292d43517541b3133a52fd73d8e8b66330f5c63
David Bachmann <dabbi@dabbi.is>
2018-10-23 14:25:23 +0000

    Final touches before initial demo of front page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,7 +7,7 @@ import SectionLegend from "../SectionLegend";
 const Hero = ({ heading, children }) => (
   <div className={styles.root}>
     <SectionLegend number="01" text="Icelandic Blockchain Foundation" />
-    <H1 bottom="xlarge" className={styles.heading}>
+    <H1 className={styles.heading}>
       Our mission is to make Iceland <br /> the leader in{" "}
       <strong>blockchain</strong> <br />
       and <strong>cryptocurrency</strong> innovation <br /> and adoption.
