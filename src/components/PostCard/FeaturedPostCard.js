6733cbcd42d1731a6cd650f7760573797e2f8208
David Bachmann <dabbi@dabbi.is>
2018-10-30 09:56:38 +0000

    Feature story image shouldn't be clickable. _blank links should have noopener norefferer.

--- src/components/PostCard/FeaturedPostCard.js
+++ src/components/PostCard/FeaturedPostCard.js
@@ -4,17 +4,17 @@ import { H2, Body3 } from "../Typography";
 import Arrow from "../Arrow";
 
 const FeaturedPostCard = ({ title, text, imageUrl, url }) => (
-  <a href={url} target="_blank" className={styles.root}>
+  <div className={styles.root}>
     <div
       className={styles.image}
       style={{ backgroundImage: `url(${imageUrl})` }}
     />
-    <div className={styles.card}>
+    <a href={url} target="blank" rel="noopener norefferer" className={styles.card}>
       <H2 bottom="small">{title}</H2>
       <Body3 light>{text}</Body3>
       <Arrow className={styles.arrow} top="medium" />
-    </div>
-  </a>
+    </a>
+  </div>
 );
 
 export default FeaturedPostCard;
