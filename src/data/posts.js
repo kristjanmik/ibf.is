bbf698b4ee23503741bd5433549a6084511fff0f
Kristjan Mikaelsson <kristjanmik@gmail.com>
2018-11-05 10:21:10 +0000

    Content changes

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,4 +1,13 @@
 export const posts = [
+
+  {
+    date: "Mon Oct 21 2018 10:30:00 GMT+0000",
+    title: "Bitcoin Anniversary Week",
+    text: "It is hard to believe that it has been ten years already, not only since the creation of Bitcoin but also the financial collapse of 2008. Cryptocurrencies and the blockchain technology have become a major player in the FinTech revolution and many of the biggest companies and institutions are in a pilot phase with products and services.",
+    image: "https://cdn-images-1.medium.com/max/1600/1*48AmBatwkM_tMUxWCvW2Og.jpeg",
+    url: "https://medium.com/icelandic-blockchain-foundation/bitcoin-anniversary-week-65eec4a89b3e",
+    isFeatured: true,
+  },
   {
     date: "Mon Oct 22 2018 10:31:49 GMT+0000",
     title: "ISX.is becomes a registered financial entity",
@@ -6,7 +15,23 @@ export const posts = [
       "A cryptocurrency exchange in Iceland received its license from the Financial Supervisory Authority that enables it to operate in full compliance of the Icelandic law.",
     image: "https://assets.ibf.is/6bf8081b458758b6f82a6c5baba4f2d4.jpg",
     url: "http://www.vb.is/frettir/skiptimynt-verdur-skradur-thjonustuveitandi/149471/",
-    isFeatured: true,
+    isFeatured: false,
+  },
+  {
+    date: "Mon Oct 20 2018 9:30:00 GMT+0000",
+    title: "IBF launches a new distributed website with IPFS 🎉",
+    text: "The Foundation is proud to announce that we have launched our newly crafted website with the help of IPFS! It was a big effort by many great people and we are happy to share the results at https://ibf.is!",
+    image: "https://cdn-images-1.medium.com/max/2000/1*MqL2UBN7_4RYf4lPi9q_xw.png",
+    url: "https://medium.com/icelandic-blockchain-foundation/ibf-launches-a-new-distributed-website-with-ipfs-5d67536816bd",
+    isFeatured: false,
+  },
+  {
+    date: "Mon Oct 20 2018 9:30:00 GMT+0000",
+    title: "The Icelandic FinTech Cluster is born",
+    text: "Wednesday was a big day for the Icelandic Blockchain Foundation. Not only did we celebrate the ten year anniversary of Bitcoin but we also became a founding member of the newly formed Icelandic FinTech Cluster. The cluster is a massive effort by Gunnlaugur Jónsson that includes some of the most powerful financial companies in Iceland, including the big banks as well as startups in the space. Overall 70 entities are participating in the effort.",
+    image: "https://cdn-images-1.medium.com/max/1600/1*hTtR4xfHM9qExcvuEKW-rQ.png",
+    url: "https://medium.com/icelandic-blockchain-foundation/the-icelandic-fintech-cluster-is-born-6614ab27f8c9",
+    isFeatured: false,
   },
   {
     date: "Mon Oct 21 2018 10:30:00 GMT+0000",
@@ -15,31 +40,5 @@ export const posts = [
     image: "//placehold.it/518x345",
     url: "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/253332782/",
     isFeatured: false,
-  },
-  {
-    date: "Mon Oct 20 2018 9:30:00 GMT+0000",
-    title: "Auroracoin strategy meeting",
-    text: "The Auroracoin team held a strategy meeting to figure out a plan for further development and expansion of the project.",
-    image: "//placehold.it/518x345",
-    url: "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255547929/",
-    isFeatured: false,
-  },
-  {
-    date: "Mon Oct 21 2018 10:30:00 GMT+0000",
-    title: "Bitcoin turns ten",
-    text: "October 31st marks the tenth anniversary of Satoshi Nakamoto releasing the inaugural Bitcoin whitepaper. With transactions worth $2 billion now carried out every single day in the cryptocurrency, Nakamoto’s Bitcoin: A Peer-to-Peer Electronic Cash System can be seen to have entirely revolutionized the global financial system.",
-    image: "//placehold.it/518x345",
-    url: "https://bitcoin.org/bitcoin.pdf",
-    isFeatured: false,
-  },
-  {
-    date: "Mon Oct 20 2018 9:30:00 GMT+0000",
-    title: "December meetup",
-    text: "The next big meetup is taking place soon, and we are looking for speakers. If you want to give a talk, please reach out to us.",
-    image: "//placehold.it/518x345",
-    url: "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255583852/",
-    isFeatured: false,
-  },
-
-
+  }
 ];
