75028a6bfb2d503237035f28b5fc297a036a94fb
Sigurdur <siggibald4@gmail.com>
2018-11-13 12:43:44 +0000

    fixing event links and desc

--- src/data/events.js
+++ src/data/events.js
@@ -25,22 +25,22 @@ export const events = [
     date: "14 nov",
     workingGroupName: "Community Engagement",
     title: "Cryptocurrency Trading Meetup",
-    subtitle: "5:30 PM GMT @ Miami Bar",
+    subtitle: "5:30 PM GMT @ TBA",
     text:
       "The Icelandic Blockchain Foundation hosts the first cryptocurrency trading meetup. More information will be announced soon",
     color: "#FF7816",
     url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255583852/",
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255583780/",
   },
   {
     date: "6 dec",
     workingGroupName: "Bitcoin",
-    title: "The Bitcoin Meetup",
-    subtitle: "8:00 PM GMT @ Hlemmur Square",
+    title: "Blockchain and Cryptocurrency Meetup by IBF",
+    subtitle: "5:30 PM GMT @ Kex Hostel",
     text:
-      "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
+      "Memorable evening of Blockchain and Cryptocurrency discussion at Kex Hostel, Reykjavík at 17:30.",
     color: "#FFD800",
     url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/253332900/",
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255583852/",
   },
 ];
