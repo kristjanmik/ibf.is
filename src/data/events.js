523f6977dc3985258a41e421b070c49f22a6ea5b
Kristjan Mikaelsson <kristjanmik@gmail.com>
2018-11-27 21:52:53 +0000

    Updated events

--- src/data/events.js
+++ src/data/events.js
@@ -1,47 +1,14 @@
 export const events = [
   {
-    date: "08 nov",
-    workingGroupName: "Community Engagement",
-    title: "Community Engagement Meeting",
-    subtitle: "8:00 PM GMT @ Miami Bar",
-    text:
-      "The community engagement committee holds its first meeting after a successful run of a crypto scavenger hunt in September.",
-    color: "#FF7816",
-    url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255358964/",
-  },
-  {
-    date: "12 nov",
-    workingGroupName: "Bitcoin",
-    title: "The Bitcoin Meetup",
-    subtitle: "8:00 PM GMT @ Hlemmur Square",
-    text:
-      "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
-    color: "#FFD800",
-    url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/253332898/",
-  },
-  {
-    date: "14 nov",
-    workingGroupName: "Community Engagement",
-    title: "Cryptocurrency Trading Meetup",
-    subtitle: "5:30 PM GMT @ Loft Hostel",
-    text:
-      "The Icelandic Blockchain Foundation hosts the first cryptocurrency trading meetup. More information will be announced soon",
-    color: "#FF7816",
-    url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255583780/",
-  },
-  {
     date: "6 dec",
     workingGroupName: "Community Engagement",
     title: "Blockchain and Cryptocurrency Meetup",
-    subtitle: "5:30 PM GMT @ Kex Hostel",
+    subtitle: "5:30 PM GMT @ TBA",
     text:
-      "Memorable evening of Blockchain and Cryptocurrency discussion at Kex Hostel, Reykjavík at 17:30.",
-    color: "#FF7816",
+      "Our bimonthly big meetup. Memorable evening of Blockchain and Cryptocurrency that starts at 5:30 PM",
+    color: "#B8D85E",
     url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255583852/",
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/255583852",
   },
   {
     date: "10 dec",
@@ -52,6 +19,72 @@ export const events = [
       "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
     color: "#FFD800",
     url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/253332900/",
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/253332900",
+  },
+  {
+    date: "13 dec",
+    workingGroupName: "Community Engagement",
+    title: "Driving engagement in Iceland - Community Meeting",
+    subtitle: "8:00 PM GMT @ TBA",
+    text:
+      "Following the success of the Cryptocurrency Scavenger Hunt, we created the Community Engagement Working Group. Last month the group discussed a mission statement as well as ways to engage Reykjavik in crypto. This month we have some decisions and actions to take! Come be an ACTIVE member of the crypto community. We are looking for a diverse set of skills and views.",
+    color: "#FF7816",
+    url:
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/256624672",
+  },
+  {
+    date: "14 jan",
+    workingGroupName: "Bitcoin",
+    title: "The Bitcoin Meetup",
+    subtitle: "8:00 PM GMT @ Hlemmur Square",
+    text:
+      "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
+    color: "#FFD800",
+    url:
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/256758058",
+  },
+  {
+    date: "11 feb",
+    workingGroupName: "Bitcoin",
+    title: "The Bitcoin Meetup",
+    subtitle: "8:00 PM GMT @ Hlemmur Square",
+    text:
+      "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
+    color: "#FFD800",
+    url:
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/256758069",
+  },
+  {
+    date: "11 mar",
+    workingGroupName: "Bitcoin",
+    title: "The Bitcoin Meetup",
+    subtitle: "8:00 PM GMT @ Hlemmur Square",
+    text:
+      "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
+    color: "#FFD800",
+    url:
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/256758075",
+  },
+  {
+    date: "8 apr",
+    workingGroupName: "Bitcoin",
+    title: "The Bitcoin Meetup",
+    subtitle: "8:00 PM GMT @ Hlemmur Square",
+    text:
+      "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
+    color: "#FFD800",
+    url:
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/256758078",
+  },
+  {
+    date: "13 may",
+    workingGroupName: "Bitcoin",
+    title: "The Bitcoin Meetup",
+    subtitle: "8:00 PM GMT @ Hlemmur Square",
+    text:
+      "A casual monthly meetup held at Hlemmur Square, Reykjavík at 20:00. Join us for a lively discussion on the blockchain, cryptocurrencies, and everything in-between!",
+    color: "#FFD800",
+    url:
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/256758090",
   },
 ];
