e11d9fc4a5d029e33a697ae13c10b36da160a929
Kristjan Mikaelsson <kristjanmik@gmail.com>
2019-02-25 11:58:58 +0000

    Updated events

--- src/data/events.js
+++ src/data/events.js
@@ -1,16 +1,5 @@
 export const events = [
   {
-    date: "25 mar",
-    workingGroupName: "Developer",
-    title: "Waves Platform Developer Meetup & Workshop",
-    subtitle: "5:30 PM GMT @ Moonlight Hall (Hard Rock)",
-    text:
-      "Waves Platform will be flying over to host a workshop and an introduction to smart contract development at the Moonlight Hall in the loft of Hard Rock Reykjavik at 17:30. Remember to bring your laptop!",
-    color: "#70D8CA",
-    url:
-      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/#",
-  },
-  {
     date: "11 mar",
     workingGroupName: "Bitcoin",
     title: "The Bitcoin Meetup",
@@ -20,6 +9,17 @@ export const events = [
     color: "#FFD800",
     url:
       "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/256758075",
+  },
+  {
+    date: "25 mar",
+    workingGroupName: "Developer",
+    title: "Waves Platform Developer Meetup & Workshop",
+    subtitle: "5:30 PM GMT @ Moonlight Hall (Hard Rock)",
+    text:
+      "Waves Platform will be flying over to host a workshop and an introduction to smart contract development at the Moonlight Hall in the loft of Hard Rock Reykjavik at 17:30. Remember to bring your laptop!",
+    color: "#70D8CA",
+    url:
+      "https://www.meetup.com/Icelandic-Blockchain-Foundation/events/#",
   },
   {
     date: "8 apr",
