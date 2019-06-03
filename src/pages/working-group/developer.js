bb610e191250241858cd322266eb09bead4d3f3b
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-06-03 22:51:28 +0000

    Placed a section in working group for a featured event

--- src/pages/working-group/developer.js
+++ src/pages/working-group/developer.js
@@ -1,24 +1,76 @@
 import React from "react";
 import Layout from "src/ui/components/Layout";
+import FeaturedPostCard from "src/ui/components/FeaturedPostCard";
 import Menu from "src/ui/components/Menu";
-import Section from "src/ui/components/Section";
 import Members from "src/ui/components/Members";
-import WorkingGroupsSection from "src/ui/sections/WorkingGroupsSection";
+import Section from "src/ui/components/Section";
 import AboutSection from "src/ui/sections/AboutSection";
-import { workingGroups, aboutDevelopersGroup } from 'src/data'
+import WorkingGroupsSection from "src/ui/sections/WorkingGroupsSection";
+import { aboutDevelopersGroup, posts, workingGroups } from "src/data";
+import { truncateText } from "src/utility/helpers/truncateText";
 
-const group = workingGroups.find(group => group.name === 'Developer')
+const group = workingGroups.find(group => group.name === "Developer");
 
 const DevelopersGroupPage = () => (
   <Layout>
     <Menu />
-    <Section top="xlarge" bottom="xlarge" backgroundText="About" number="03" legendText="About" legendTextColor="dark">
+    <Section
+      top="xlarge"
+      bottom="xlarge"
+      backgroundText="About"
+      number="01"
+      legendText="About"
+      legendTextColor="dark"
+    >
+      {/* TODO: check if I should extend AboutSection for image } */}
       <AboutSection data={aboutDevelopersGroup} />
     </Section>
-    <Section top="xlarge" bottom="xlarge" backgroundText="board" number="02" legendText="Members" legendTextColor="dark">
+    <Section
+      top="medium"
+      bottom="xlarge"
+      backgroundText="events"
+      // xid="posts"
+      number="02"
+      legendText="Next event in the group"
+      legendTextColor="dark"
+      footerText="see more events"
+      footerTextColor="dark"
+      footerUrl="https://link-to-meetups"
+    >
+      {/* TODO: change content } */}
+      {posts
+        .filter(post => post.isFeatured)
+        .slice(0, 1)
+        .map(post => (
+          <FeaturedPostCard
+            key={post.title}
+            title={post.title}
+            text={truncateText.apply(post.text)}
+            imageUrl={post.image}
+            date={post.date}
+            url={post.url}
+            fullWidth
+          />
+        ))}
+    </Section>
+    <Section
+      top="xlarge"
+      bottom="xlarge"
+      backgroundText="board"
+      number="03"
+      legendText="Members"
+      legendTextColor="dark"
+    >
       <Members of={group} />
     </Section>
-    <Section colored top="xlarge" bottom="xlarge" backgroundText="groups" number="03" legendText="Working Groups">
+    <Section
+      colored
+      top="xlarge"
+      bottom="xlarge"
+      backgroundText="groups"
+      number="04"
+      legendText="Working Groups"
+    >
       <WorkingGroupsSection groups={workingGroups} />
     </Section>
   </Layout>
