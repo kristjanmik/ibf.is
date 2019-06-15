1c69ae9ea65dfe8738b7c89c08a63ec43e4a97a6
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-06-15 21:11:59 +0000

    Revert changes done to FeaturedPostCard

--- src/ui/components/FeaturedPostCard/FeaturedPostCard.js
+++ src/ui/components/FeaturedPostCard/FeaturedPostCard.js
@@ -1,20 +1,10 @@
 import React from "react";
 import styles from "./FeaturedPostCard.module.scss";
-import DateCube from "src/ui/components/DateCube";
-import { H2, Body3, Body4 } from "../Typography";
+import { H2, Body3 } from "../Typography";
 import Arrow from "../Arrow";
 
-const FeaturedPostCard = ({
-  color,
-  date,
-  fullWidth,
-  imageUrl,
-  text,
-  title,
-  subtitle,
-  url,
-}) => (
-  <div className={`${styles.root} ${fullWidth ? styles.fullWidth : ""}`}>
+const FeaturedPostCard = ({ imageUrl, text, title, url }) => (
+  <div className={`${styles.root}`}>
     <div
       className={styles.image}
       style={{ backgroundImage: `url(${imageUrl})` }}
@@ -22,18 +12,12 @@ const FeaturedPostCard = ({
     <a
       href={url}
       target="blank"
-      rel="noopener noreferrer"
+      rel="noopener wnoreferrer"
       className={styles.card}
     >
-      {fullWidth && <DateCube date={date} color={color} />}
       <H2 bottom="small">{title}</H2>
-      {fullWidth && (
-        <Body4 bottom="small" className={styles.subtitle}>
-          {subtitle}
-        </Body4>
-      )}
       <Body3 light>{text}</Body3>
-      {fullWidth || <Arrow className={styles.arrow} top="medium" />}
+      <Arrow className={styles.arrow} top="medium" />
     </a>
   </div>
 );
